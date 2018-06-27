becdd4a46b6ea9e2a8e65b68873ddef7d7b6201d
Phanosek <phanosek@seznam.cz>
2018-06-27 18:54:14 +0200

    MUJ CTVERECEK

---
+++ rothko/empty-example/sketch.js
@@ -0,0 +1,13 @@
+// do not remove, allowes autocomplete
+/// <reference path="./../p5.global-mode.d.ts" />
+
+function setup() {
+  createCanvas(50, 50)
+
+  fill('yellow')
+  rect(20, 20, 50, 50)
+}
+
+function draw() {
+  // put drawing code here
+}
