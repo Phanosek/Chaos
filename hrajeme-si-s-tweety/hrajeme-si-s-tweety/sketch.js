06953d90c56a19c2b8570cfc136ed528d1181f57
Phanosek <phanosek@seznam.cz>
2018-06-30 11:14:46 +0200

    Hokus pokus

--- hrajeme-si-s-tweety/hrajeme-si-s-tweety/sketch.js
+++ hrajeme-si-s-tweety/hrajeme-si-s-tweety/sketch.js
@@ -1,20 +1,74 @@
 // do not remove, allowes autocomplete
 /// <reference path="./../p5.global-mode.d.ts" />
 
-function hiphop(tweet) {
-  console.log(tweet.text,replace('rap));
+
+var img;
+function preload() {
+  img = loadImage('Europe.jpg');
 }
 
-function Hampl(tweet) {
-  return tweet.text.match(/PetrHam/);
-};
-
 function setup() {
-  var vysledek = tweets.filter(Hampl);
   
-  console.log(vysledek);
+createCanvas(2000,2000);
+angleMode(DEGREES);
+colorMode(HSB);
+background('red');
 }
 
 function draw() {
-  // put drawing code here
+  image(img, 0, 0);
+
+fill('green');
+ellipse(430, 600, 50, 50);
+
+
+
+fill('red');
+ellipse(280, 700, tabulka [5][1], [5][1]);
+
+
+fill('blue');
+ellipse(244, 507, tabulka [26][1], [26][1]);
+
+fill('blue');
+ellipse(430, 330, 50, 50);
+
+fill('pink');
+strokeWeight(0);
+arc(430, 330, 50, 50, 80, PI + QUARTER_PI, PIE);
+
+  text(mouseX+' '+mouseY, mouseX, mouseY);
+
 }
+
+var tabulka = [[654, 541/10, 113, 'Belgium'],
+[27.300, 25.784, 1.516, 'Bulgaria'],
+[273, 209, 64, 'Denmark'],
+[188, 165, 23, 'Estonia'],
+[357, 265, 92, 'Finland'],
+[3.177, 2677/20, 500, 'France'],
+[1.295, 936, 359, 'Croatia'],
+[562, 384, 178, 'Ireland'],
+[3.473, 2.600, 873, 'Italy'],
+[65, 49, 16, 'Cyprus'],
+[1.363, 1.235, 128, 'Lithuania'],
+[707, 604, 103, 'Latvia'],
+[24, 17, 7, 'Luxembourg'],
+[11.342, 10.766, 576, 'Hungary'],
+[37, 28, 9, 'Malta'],
+[7.092, 3.983, 3.109, 'Germany'],
+[1.376, 1.104, 272, 'Netherlands'],
+[33.623, 31.355, 2.268, 'Poland'],
+[687, 621, 66, 'Portugal'],
+[1.605, 1.119, 486, 'Austria'],
+[32.776, 31.522, 1.254, 'Romania'],
+[966, 779, 187, 'Greece'],
+[177.001, 161.559, 15.442, 'Slovakia'],
+[294, 228, 66, 'Slovenia'],
+[1.898, 1.610, 288, 'Spain'],
+[502, 404, 98, 'Sweden'],
+[4.903, 3300/20, 1.603, 'United Kingdom']];
+
+tabulka [0]
+console.log(tabulka [25]);
+
